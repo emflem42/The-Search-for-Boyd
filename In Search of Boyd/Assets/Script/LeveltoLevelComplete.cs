c1d3ae6789fb8186d43bfcb891a2c8bbc620efb5
yee133 <yee133@mail.chapman.edu>
2014-12-14 15:01:23 -0800

    Level Complete Transition Scripts

---
+++ In Search of Boyd/Assets/Script/LeveltoLevelComplete.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeveltoLevelSelect : MonoBehaviour {
+
+	public LevelBeat beat;
+	public int levelID;
+	public int levelCompleteID;
+
+	// Use this for initialization
+	void Start () {
+		beat = GameObject.Find ("LevelBeatObject").GetComponent<LevelBeat> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		/*if (Input.GetKey(KeyCode.Return))
+		{
+			if (beat.levelBeat == levelID)
+			{
+				beat.levelBeat++;
+			}
+			Application.LoadLevel (1);
+		}*/
+	}
+
+	void OnTriggerEnter2D (Collider2D other)
+	{
+		if (beat.levelBeat == levelID)
+		{
+			beat.levelBeat++;
+		}
+
+		Application.LoadLevel (levelCompleteID); //Make sure this goes to LevelComplete
+	}
+}
